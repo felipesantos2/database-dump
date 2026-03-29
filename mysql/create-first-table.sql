c0227fdf23fdebaab23924043f56a16dd2b148fa
felipesantos2 <santospinheiro627@gmail.com>
2026-03-29 09:41:48 -0300

    wip

---
+++ mysql/create-first-table.sql
@@ -0,0 +1,12 @@
+use test_db;
+create table users (
+    id int auto_increment primary key,
+    name varchar(200) null,
+    email varchar(200) null unique,
+  	deleted_at datetime null,
+  	created_at datetime null default current_timestamp, 
+    updated_at datetime null default current_timestamp on update current_timestamp  
+);
+
+insert into users (name, email) values('Felipe', 'felipe@email.com');
+insert into users (name, email) values('Admin', 'admin@email.com');
